7f4a6ba6a2a1db770b509c3155176158af5d9f3f
张均浩 <1134488372@qq.com>
2020-09-19 09:37:48 +0800

    创建utils工具类函数文件夹，创建axios实例

---
+++ src/utils/request.js
@@ -0,0 +1,22 @@
+import axios from 'axios'
+import Vue from 'vue'
+import router from '../router'
+
+const request = axios.create({
+	baseURL: 'http://localhost:3005/v1/api',
+	timeout: 5000
+})
+
+request.interceptors.response.use(res => {
+	return res
+}, error => {
+	if (error.response.data.message) {
+		Vue.prototype.$message.error(error.response.data.message)
+	}
+	if (error.response.status === 401) {
+		router.push('/login')
+	}
+	return Promise.reject(error)
+})
+
+export default request
